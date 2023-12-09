96176993c50415786569a5e773908621b952e1a5
Prakhar Nagore <prakharnagore111@gmail.com>
2023-12-09 19:18:38 +0530

    question card added

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import RenderTag from "../shared/RenderTag";
 import Metric from "../shared/Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
 
 interface QuestionProps {
   _id: string;
@@ -33,7 +34,7 @@ const QuestionCard = ({
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
-            {String(createdAt)}
+            {getTimestamp(createdAt)}
           </span>
           <Link href={`/questions/${_id}`}>
             <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
@@ -50,10 +51,33 @@ const QuestionCard = ({
       </div>
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
+          imgUrl="/assets/icons/avatar.svg"
+          alt="user"
+          value={author.name}
+          title={` - asked ${getTimestamp(createdAt)} ago`}
+          href={`/profile/${author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
+        />
+        <Metric
           imgUrl="/assets/icons/like.svg"
           alt="Upvotes"
-          value={upvotes}
+          value={formatAndDivideNumber(upvotes)}
           title=" Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={formatAndDivideNumber(answers.length)}
+          title=" Answer"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={formatAndDivideNumber(views)}
+          title=" Views"
           textStyles="small-medium text-dark400_light800"
         />
       </div>
--- components/shared/Metric.tsx
+++ components/shared/Metric.tsx
@@ -1,16 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-interface Props {
+interface MetricProps {
   imgUrl: string;
   alt: string;
-  value: string;
+  value: string | number;
   title: string;
+  href?: string;
   textStyles: string;
-  isAuthor: string;
+  isAuthor?: Boolean;
 }
 
-const Metric = ({ imgUrl, alt, value, title, textStyles, isAuthor }: Props) => {
-  return <div>Metric</div>;
+const Metric = ({
+  imgUrl,
+  alt,
+  value,
+  title,
+  href,
+  textStyles,
+  isAuthor,
+}: MetricProps) => {
+  const merticContent = (
+    <>
+      <Image
+        src={imgUrl}
+        height={16}
+        width={16}
+        alt={alt}
+        className={`object-contain ${href ? "rounded-full" : ""}`}
+      />
+      <p className={`${textStyles} flex items-center gap-1`}>
+        {value}
+        <span
+          className={`small-regular line-clamp-1 ${
+            isAuthor ? "max-sm:hidden" : ""
+          }`}
+        >
+          {title}
+        </span>
+      </p>
+    </>
+  );
+
+  if (href) {
+    return (
+      <Link href={href} className="flex-center gap-1">
+        {merticContent}
+      </Link>
+    );
+  }
+
+  return <div className="flex-center flex-wrap gap-1">{merticContent}</div>;
 };
 
 export default Metric;
