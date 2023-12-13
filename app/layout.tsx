9a6abc1808a013508c00991396dcc810fa2ae42a
Prakhar Nagore <prakharnagore111@gmail.com>
2023-12-14 02:16:49 +0530

    disable_eslint_camelcase_issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
