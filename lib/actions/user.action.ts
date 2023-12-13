9f97bcc75f87d6d596628ac018eccecea413ca1c
Prakhar Nagore <prakharnagore111@gmail.com>
2023-12-14 02:03:56 +0530

    webhooks_implementation

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.modal";
 
 export async function getUserById(params) {
   try {
@@ -11,6 +18,66 @@ export async function getUserById(params) {
       clearId: userId,
     });
     return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId, updateData, path } = params;
+    await User.findOneAndUpdate(
+      {
+        clerkId,
+      },
+      updateData,
+      {
+        new: true,
+      }
+    );
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId } = params;
+
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    // Delete user from database
+    // and questions, answers, comments, etc.
+    const userQuestionIds = await Question.find({
+      author: user._id,
+    }).distinct("_id");
+
+    console.log(userQuestionIds);
+
+    await Question.deleteMany({
+      author: user._id,
+    });
+
+    const deleteUser = await User.findByIdAndDelete(user._id);
+    return deleteUser;
   } catch (error) {
     console.log(error);
     throw error;
