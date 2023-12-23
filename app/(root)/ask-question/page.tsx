f71e9c89fafba2433ba5cc946e6dfbc39f7abbbc
Prakhar Nagore <prakharnagore111@gmail.com>
2023-12-24 01:04:33 +0530

    webhooks_implementation

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,13 +1,12 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import React from "react";
 
 const AskQuestion = async () => {
-  // const { userId } = auth();
-  const userId = "CL123456";
+  const { userId } = auth();
   if (!userId) redirect("/sign-in");
   const mongoUser = await getUserById({ userId });
 
