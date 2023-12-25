3c7d459db1e9b853d0838869b1d13f0d51d8a9d5
Prakhar Nagore <prakharnagore111@gmail.com>
2023-12-25 15:26:18 +0530

    fix build error

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -7,7 +7,10 @@ import { GetTopInteractedTagsParams } from "./shared.types";
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
     connectToDatabase();
-    const { userId, limit = 3 } = params;
+    const {
+      userId,
+      // limit = 3
+    } = params;
     const user = await User.findById(userId);
 
     if (!user) throw new Error("User not found");
