f0fb42cd303ec0564480344aa32dca414993b8b9
Prakhar Nagore <prakharnagore111@gmail.com>
2023-12-25 15:22:37 +0530

    community_page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from "../mongoose";
 import {
   CreateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
@@ -54,6 +55,7 @@ export const updateUser = async (params: UpdateUserParams) => {
     throw error;
   }
 };
+
 export const deleteUser = async (params: DeleteUserParams) => {
   try {
     connectToDatabase();
@@ -87,3 +89,15 @@ export const deleteUser = async (params: DeleteUserParams) => {
     throw error;
   }
 };
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+    const users = await User.find({}).sort({ createdAt: -1 });
+    return { users };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
