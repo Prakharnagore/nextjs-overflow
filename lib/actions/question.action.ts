f2491dd25e35d0814b1781a7106053ea6b751cbc
Prakhar Nagore <prakharnagore111@gmail.com>
2024-01-16 23:09:59 +0530

    Integrate filters on home page

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -20,7 +20,8 @@ import { FilterQuery } from "mongoose";
 export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
-    const { searchQuery } = params;
+    const { searchQuery, filter } = params;
+
     const query: FilterQuery<typeof Question> = {};
 
     if (searchQuery) {
@@ -29,10 +30,29 @@ export async function getQuestions(params: GetQuestionsParams) {
         { content: { $regex: new RegExp(searchQuery, "i") } },
       ];
     }
+    let sortOptions = {};
+
+    switch (filter) {
+      case "newest":
+        sortOptions = { createdAt: -1 };
+        break;
+
+      case "frequent":
+        sortOptions = { views: -1 };
+        break;
+
+      case "unanswered":
+        query.answers = { $size: 0 };
+        break;
+
+      default:
+        break;
+    }
+
     const questions = await Question.find(query)
       .populate({ path: "tags", model: Tag })
       .populate({ path: "author", model: User })
-      .sort({ createdAt: -1 });
+      .sort(sortOptions);
     return { questions };
   } catch (error) {
     console.log(error);
